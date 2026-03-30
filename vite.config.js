d960af9b3035013d1a0e9d7552a9fe9331b5e9e6
YangySun <775959301@qq.com>
2026-03-30 10:24:33 +0800

    Fix 404 error: use different base path for dev and production

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/blog/',
+  base: process.env.NODE_ENV === 'production' ? '/blog/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
