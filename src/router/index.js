08301614b697a0a951a1f56c4106a3982c0ec685
YangySun <775959301@qq.com>
2026-03-30 10:50:12 +0800

    Fix 404 error: add /blog/ prefix to all routes and base path

--- src/router/index.js
+++ src/router/index.js
@@ -2,27 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/',
+    path: '/blog/',
     name: 'Home',
     component: () => import('../views/Home.vue')
   },
   {
-    path: '/articles',
+    path: '/blog/articles',
     name: 'Articles',
     component: () => import('../views/Articles.vue')
   },
   {
-    path: '/article/:id',
+    path: '/blog/article/:id',
     name: 'ArticleDetail',
     component: () => import('../views/ArticleDetail.vue')
   },
   {
-    path: '/about',
+    path: '/blog/about',
     name: 'About',
     component: () => import('../views/About.vue')
   },
   {
-    path: '/links',
+    path: '/blog/links',
     name: 'Links',
     component: () => import('../views/Links.vue')
   }
